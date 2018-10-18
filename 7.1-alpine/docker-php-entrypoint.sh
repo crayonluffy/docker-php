5a54b9245f55aac4b6703f6e63631cb105100f13
Oanh Nguyen <oanhnn.bk@gmail.com>
2018-10-18 10:57:56 +0700

    Update

--- 7.1-alpine/docker-php-entrypoint.sh
+++ 7.1-alpine/docker-php-entrypoint.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 set -e
 
-# Enable or Disable XDebug
+# Enable XDebug
 if [ "${PHP_ENABLE_XDEBUG:-0}" = "1" ]; then
     sed -i "s|;zend_extension=xdebug.so|zend_extension=xdebug.so|i" /etc/php/conf.d/xdebug.ini
-else
-    sed -i "s|zend_extension=xdebug.so|;zend_extension=xdebug.so|i" /etc/php/conf.d/xdebug.ini
 fi
 
 # first arg is `-f` or `--some-option`
